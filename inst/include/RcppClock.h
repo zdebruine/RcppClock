aa19372520dcc28125e3b6af01a0ada2aa4fa1d0
zdebruine <zacharydebruine@gmail.com>
2021-11-25 09:16:15 -0500

    faster performance for large benchmarking runs (i.e. > 1MB)

--- inst/include/RcppClock.h
+++ inst/include/RcppClock.h
@@ -3,52 +3,36 @@
 
 namespace Rcpp {
   class Clock {
-  private:
-    std::vector<std::chrono::steady_clock::time_point> tick_times, tock_times;
-    std::vector<std::string> tick_names, tock_names, unique_tickers;
+    private:
+    std::vector<std::chrono::steady_clock::time_point> tick_times;
+    std::vector<std::string> tick_names, durations_names;
+    std::vector<double> durations;
 
-  public:
+    public:
+
     // start a timer
     void tick(std::string name) {
       tick_names.push_back(name);
-      tick_times.push_back(std::chrono::steady_clock::now());
+      tick_times.resize(tick_times.size() + 1);
+      tick_times.back() = std::chrono::steady_clock::now();
     }
 
     // stop a timer
     void tock(std::string name) {
-      tock_names.push_back(name);
-      tock_times.push_back(std::chrono::steady_clock::now());
+      std::chrono::steady_clock::time_point tock_time = std::chrono::steady_clock::now();
+      durations_names.push_back(name);
+      for (size_t i = tick_names.size(); i > 0; --i) {
+        if (tick_names[i - 1] == name) {
+          durations.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(tock_time - tick_times[i - 1]).count());
+          return;
+        }
+      }
+      Rcpp::stop("Rcpp::Clock 'tock' with name of '" + name + "' did not match with a corresponding 'tick' name");
     }
 
     // calculate timer durations
     void stop(std::string var_name) {
-
-      // create a timers vector holding runtimes for each tick/tock pair
-      size_t num_ticks = tick_names.size();
-      std::vector<double> timers(num_ticks);
-      std::vector<std::string> tickers(num_ticks);
-
-      // find unique ticker names
-      for (size_t i = 0; i < num_ticks; ++i)
-        if (std::find(unique_tickers.begin(), unique_tickers.end(), tick_names[i]) == unique_tickers.end())
-          unique_tickers.push_back(tick_names[i]);
-
-      // map all tick_names to tock_names and compute corresponding duration between tick_times and tock_times indices
-      size_t i_timer = 0, i_tick = 0;
-      for (auto ticker : unique_tickers) {
-        for (size_t i = 0, j = 0; i < num_ticks; ++i) {
-          if (tick_names[i] == ticker) {
-            // find next tock_names/tock_times index corresponding to "ticker"
-            while (tock_names[j] != ticker) ++j;
-            tickers[i_tick] = ticker;
-            timers[i_timer] = std::chrono::duration_cast<std::chrono::nanoseconds>(tock_times[j] - tick_times[i]).count();
-            ++j;
-            ++i_tick;
-            ++i_timer;
-          }
-        }
-      }
-      DataFrame df = DataFrame::create(Named("ticker") = tickers, Named("timer") = timers);
+      DataFrame df = DataFrame::create(Named("ticker") = durations_names, Named("timer") = durations);
       df.attr("class") = "RcppClock";
       Environment env = Environment::global_env();
       env[var_name] = df;
