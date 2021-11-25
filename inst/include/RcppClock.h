466b1d885fcf2fae2f64d7c5fd2fd745caf58c5d
zdebruine <zacharydebruine@gmail.com>
2021-11-24 21:28:27 -0500

    Add files via upload

--- inst/include/RcppClock.h
+++ inst/include/RcppClock.h
@@ -1,42 +1,50 @@
 #include <Rcpp.h>
 #include <chrono>
 
-#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
-#define now() std::chrono::high_resolution_clock::now()
-
 namespace Rcpp {
   class Clock {
   private:
-    std::vector<std::chrono::high_resolution_clock::time_point> tick_times;
-    std::vector<std::chrono::high_resolution_clock::time_point> tock_times;
-    std::vector<std::string> tick_names;
-    std::vector<std::string> tock_names;
-    std::vector<double> timers;
-    std::vector<std::string> tickers;
+    std::vector<std::chrono::steady_clock::time_point> tick_times, tock_times;
+    std::vector<std::string> tick_names, tock_names, unique_tickers;
 
   public:
     // start a timer
     void tick(std::string name) {
       tick_names.push_back(name);
-      tick_times.push_back(now());
+      tick_times.push_back(std::chrono::steady_clock::now());
     }
 
     // stop a timer
     void tock(std::string name) {
       tock_names.push_back(name);
-      tock_times.push_back(now());
+      tock_times.push_back(std::chrono::steady_clock::now());
     }
 
     // calculate timer durations
     void stop(std::string var_name) {
-      for (unsigned int i = 0; i < tick_names.size(); ++i) {
-        for (unsigned int j = 0; j < tock_names.size(); ++j) {
-          if (tick_names[i] == tock_names[j]) {
-            timers.push_back(duration(tock_times[j] - tick_times[i]));
-            tickers.push_back(tick_names[i]);
-            tock_times.erase(tock_times.begin() + j);
-            tock_names.erase(tock_names.begin() + j);
-            break;
+
+      // create a timers vector holding runtimes for each tick/tock pair
+      size_t num_ticks = tick_names.size();
+      std::vector<double> timers(num_ticks);
+      std::vector<std::string> tickers(num_ticks);
+
+      // find unique ticker names
+      for (size_t i = 0; i < num_ticks; ++i)
+        if (std::find(unique_tickers.begin(), unique_tickers.end(), tick_names[i]) == unique_tickers.end())
+          unique_tickers.push_back(tick_names[i]);
+
+      // map all tick_names to tock_names and compute corresponding duration between tick_times and tock_times indices
+      size_t i_timer = 0, i_tick = 0;
+      for (auto ticker : unique_tickers) {
+        for (size_t i = 0, j = 0; i < num_ticks; ++i) {
+          if (tick_names[i] == ticker) {
+            // find next tock_names/tock_times index corresponding to "ticker"
+            while (tock_names[j] != ticker) ++j;
+            tickers[i_tick] = ticker;
+            timers[i_timer] = std::chrono::duration_cast<std::chrono::nanoseconds>(tock_times[j] - tick_times[i]).count();
+            ++j;
+            ++i_tick;
+            ++i_timer;
           }
         }
       }
